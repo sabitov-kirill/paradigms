1712d5b83e15637102084e84d687ca7403e278a4
Sabitov Kirill <sabitovkirill@ya.ru>
2023-03-15 22:49:35 +0300

    HW6 done.

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -4,8 +4,8 @@
 
 let cnst = value => () => value;
 let variable = (name) => (x, y, z) => ({ x, y, z })[name];
-let operator = f => (...operators) => (x, y, z) => f(...operators.map(op => op(x, y, z)));
 
+let operator = f => (...expressions) => (x, y, z) => f(...expressions.map(expression => expression(x, y, z)));
 let add = operator((x, y) => x + y);
 let subtract = operator((x, y) => x - y);
 let multiply = operator((x, y) => x * y);
@@ -18,24 +18,22 @@ let negate = operator(x => -x);
 
 let parse = (source) => {
     const variableNames = ['x', 'y', 'z'];
-    const binaryOperators = {
-        '+': add,
-        '-': subtract,
-        '*': multiply,
-        '/': divide
-    };
-    const unaryOperators = {
-        'negate': negate
+    const operators = {
+        '+':      { operation: add,      arity: 2 },
+        '-':      { operation: subtract, arity: 2 },
+        '*':      { operation: multiply, arity: 2 },
+        '/':      { operation: divide,   arity: 2 },
+        'negate': { operation: negate,   arity: 1 },
     };
 
     let stack = [];
     source.trim().split(/\s+/).forEach(token => {
         let result;
-        if (token in binaryOperators) {
-            let last = stack.pop(), first = stack.pop();
-            result = binaryOperators[token](first, last);
-        } else if (token in unaryOperators) {
-            result = unaryOperators[token](stack.pop());
+        if (token in operators) {
+            result = operator.operation(...stack.splice(
+                stack.length - operators[token].arity,
+                operators[token].arity
+            ));
         } else if (variableNames.includes(token)) {
             result = variable(token);
         } else {
@@ -43,9 +41,7 @@ let parse = (source) => {
         }
 
         stack.push(result);
-    })
+    });
 
     return stack[0];
 }
-
-parse("x x 2 - * x * 1 +")
