486b379459ef79e97580c8c73d03b02319fb9dcf
Sabitov Kirill <sabitovkirill@ya.ru>
2023-03-23 17:04:44 +0300

    HW6 mods done.

--- java-solutions/expression/generic/Main.java
+++ java-solutions/expression/generic/Main.java
@@ -1,8 +1,11 @@
 package expression.generic;
 
+/*
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        assert args.length > 2 : "Not enough command line arguments.";
+
         Object[][][] values = new GenericTabulator().tabulate(
                 args[0].substring(1),
                 args[1],
@@ -16,4 +19,77 @@ public class Main {
             }
         }
     }
+}
+*/
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    private static boolean check(int[] arr1, int[] arr2) {
+        int n = arr1.length;
+        for (int a : arr1) {
+            for (int b : arr2) {
+                var set1 = set(arr1, a);
+                var set2 = set(arr2, b);
+
+                int s1 = set1.size();
+                int s2 = set2.size();
+
+                set1.retainAll(set2);
+
+                int intSize = set1.size();
+
+                if (intSize * n != s1 * s2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static Set<Integer> set(int[] arr, int a) {
+        Set<Integer> s = new TreeSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= a) {
+                s.add(i);
+            }
+        }
+
+        return s;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(Files.newBufferedReader(Path.of("data4_demo.txt")));
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[j][i] = scanner.nextInt();
+            }
+        }
+
+        int res = 0;
+
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = i + 1; j < m; j++) {
+                if (check(arr[i], arr[j])) {
+                    res++;
+
+                    if(i + 1 == 1) {
+                        System.out.println(j + 1);
+                    }
+                }
+            }
+        }
+
+        System.out.println(res);
+    }
+
 }
--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -2,42 +2,78 @@
  * Operators
  *=============================================*/
 
-let cnst = value => () => value;
-let variable = (name) => (x, y, z) => ({ x, y, z })[name];
+const variable = (name) => (x, y, z) => ({ x, y, z })[name];
 
-let operator = f => (...expressions) => (x, y, z) => f(...expressions.map(expression => expression(x, y, z)));
-let add = operator((x, y) => x + y);
-let subtract = operator((x, y) => x - y);
-let multiply = operator((x, y) => x * y);
-let divide = operator((x, y) => x / y);
-let negate = operator(x => -x);
+const cnst = value => () => value;
+const one = cnst(1);
+const two = cnst(2);
+const pi = cnst(Math.PI);
+const e = cnst(Math.E);
+
+const operator = f => (...expressions) => (x, y, z) => f(...expressions.map(expression => expression(x, y, z)));
+
+const arrayIndexByRelation = (binaryRelation, size) => operator((...array) => array.slice(0, size).reduce(
+    (bestIndex, value, index) => binaryRelation(value, array[bestIndex]) ? bestIndex : index, -1
+));
+const argMin3 = arrayIndexByRelation((x, y) => x >= y, 3);
+const argMax3 = arrayIndexByRelation((x, y) => x <= y, 3);
+const argMin5 = arrayIndexByRelation((x, y) => x >= y, 5);
+const argMax5 = arrayIndexByRelation((x, y) => x <= y, 5);
+
+const madd = operator((x, y, z) => x * y + z);
+const add = operator((x, y) => x + y);
+const subtract = operator((x, y) => x - y);
+const multiply = operator((x, y) => x * y);
+const divide = operator((x, y) => x / y);
+
+const negate = operator(x => -x);
+const floor = operator(Math.floor);
+const ceil = operator(Math.ceil);
+const sin = operator(Math.sin);
+const sinh = operator(Math.sinh);
+const cos = operator(Math.cos);
+const cosh = operator(Math.cosh);
 
 /*=============================================
  * Expression parser
  *=============================================*/
 
 let parse = (source) => {
+    const cnstAliases = { one, two };
     const variableNames = ['x', 'y', 'z'];
     const operators = {
-        '+':      { operation: add,      arity: 2 },
-        '-':      { operation: subtract, arity: 2 },
-        '*':      { operation: multiply, arity: 2 },
-        '/':      { operation: divide,   arity: 2 },
-        'negate': { operation: negate,   arity: 1 },
+        'argMin5': { operation: argMin5,  arity: 5 },
+        'argMax5': { operation: argMax5,  arity: 5 },
+        'argMin3': { operation: argMin3,  arity: 3 },
+        'argMax3': { operation: argMax3,  arity: 3 },
+        '*+':      { operation: madd,     arity: 3 },
+        '+':       { operation: add,      arity: 2 },
+        '-':       { operation: subtract, arity: 2 },
+        '*':       { operation: multiply, arity: 2 },
+        '/':       { operation: divide,   arity: 2 },
+        'negate':  { operation: negate,   arity: 1 },
+        '_':       { operation: floor,    arity: 1 },
+        '^':       { operation: ceil,     arity: 1 },
+        'sin':     { operation: sin,      arity: 1 },
+        'sinh':    { operation: sinh,      arity: 1 },
+        'cosh':    { operation: cosh,      arity: 1 },
+        'cos':     { operation: cos,      arity: 1 },
     };
 
     let stack = [];
     source.trim().split(/\s+/).forEach(token => {
         let result;
         if (token in operators) {
-            result = operator.operation(...stack.splice(
+            result = operators[token].operation(...stack.splice(
                 stack.length - operators[token].arity,
                 operators[token].arity
             ));
         } else if (variableNames.includes(token)) {
             result = variable(token);
         } else {
-            result = cnst(parseInt(token));
+            result = token in cnstAliases ?
+                cnstAliases[token] :
+                cnst(parseFloat(token));
         }
 
         stack.push(result);
