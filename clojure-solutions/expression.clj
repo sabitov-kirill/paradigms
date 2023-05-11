b03bbbfada95de71b06d6b2dd1ff12ce07e31648
Sabitov Kirill <sabitovkirill@ya.ru>
2023-05-11 17:05:47 +0300

    HW12 done.

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -38,18 +38,18 @@
 
 (deftype Const [value]
   IExpression
-  (evaluate [this _] (.-value this))
-  (toStringPostfix [this] (str (.-value this)))
+  (evaluate [_ _] value)
+  (toStringPostfix [_] (str value))
   Object
-  (toString [this] (str (.-value this))))
+  (toString [_] (str value)))
 (defn Constant [value] (Const. value))
 
 (deftype Var [name]
   IExpression
-  (evaluate [this vars] (vars (.-name this)))
-  (toStringPostfix [this] (str (.-name this)))
+  (evaluate [_ vars] (vars ((comp clojure.string/lower-case first str) name)))
+  (toStringPostfix [_] name)
   Object
-  (toString [this] (str (.-name this))))
+  (toString [_] name))
 (defn Variable [name] (Var. name))
 
 (deftype Expression [f sign args]
@@ -69,6 +69,9 @@
 (defn Cosh [& args] (Expression. math/cosh "cosh" args))
 (defn Atan [& args] (Expression. math/atan "atan" args))
 (defn Atan2 [& args] (Expression. math/atan2 "atan2" args))
+(defn UPow [& args] (Expression. math/exp "**" args))
+(defn ULog [& args] (Expression. math/log "//" args))
+
 (def ArcTan Atan)
 (def ArcTan2 Atan2)
 
@@ -76,8 +79,19 @@
 (defn toString [expr] (.toString expr))
 (defn toStringPostfix [expr] (.toStringPostfix expr))
 
-(def ops-object {'+   Add '- Subtract '* Multiply '/ Divide 'negate Negate
-                 'sin Sin 'cos Cos 'sinh Sinh 'cosh Cosh 'atan Atan 'atan2 Atan2})
+(def ops-object {'+            Add
+                 '-            Subtract
+                 '*            Multiply
+                 '/            Divide
+                 'negate       Negate
+                 'sin          Sin
+                 'cos          Cos
+                 'sinh         Sinh
+                 'cosh         Cosh
+                 'atan         Atan
+                 'atan2        Atan2
+                 '**           UPow
+                 (symbol "//") ULog})
 (def parseObject (parser Constant Variable ops-object))
 
 ; Combinator parser
@@ -89,16 +103,16 @@
            *digit (*chars #(Character/isDigit %))
            *number (+str (+plus *digit))
            *const (+seqf (comp Constant read-string str) (+opt (+or \- \+)) *number \. *number)
-           *variable (+map (comp Variable str) (+char "xyz"))
+           *variable (+map Variable (+str (+plus (+char "xyzXYZ"))))
            (*identifier [name] (+str (apply +seq (map +char (clojure.string/split name #"")))))
            *operator (+map (comp ops-object symbol) (apply +or (map (comp *identifier str) (keys ops-object))))
            (*char-ignore [chs] (+ignore (+char chs)))
            *space (*chars #(Character/isWhitespace %))
            *ws (+ignore (+star *space))
            *value (+seqn 1
-                    (+char "(") *ws
-                    (+seq (+star (+seqn 0 (+or *const *variable (delay *expression)) *ws)) *operator)
-                    *ws (+char ")"))
+                         (+char "(") *ws
+                         (+seq (+star (+seqn 0 (+or *const *variable (delay *expression)) *ws)) *operator)
+                         *ws (+char ")"))
            *expression (+map (fn [[args op]] (apply op args)) *value)
-           *parseObjectPostfix (+seqn 0 *ws(+or *const *variable *expression) *ws))
+           *parseObjectPostfix (+seqn 0 *ws (+or *const *variable *expression) *ws))
 
